a8df200ba5b846368a17d63b48bc1a64ff9893bf
Daniel Kahoun <113265820+danielkahoun@users.noreply.github.com>
2022-12-29 19:06:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    data() {
+        return {
+            hexColor: null
+        }
+    },
+    methods: {
+        updateBackgroundColor() {
+            const date = new Date();
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
+
+            hours = hours.toString().length === 1 ? '0' + hours : hours;
+            minutes = minutes.toString().length === 1 ? '0' + minutes : minutes;
+            seconds = seconds.toString().length === 1 ? '0' + seconds : seconds;
+
+            document.body.style.backgroundColor = this.hexColor = '#' + hours + minutes + seconds;
+        }
+    },
+    created() {
+        this.updateBackgroundColor();
+    },
+    mounted: function() {
+        setInterval(this.updateBackgroundColor, 1000)
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1 id="clock">{{hexColor}}</h1>
+    </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Exo+2:wght@100&display=swap');
+
+.container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#clock {
+    font-family: 'Exo 2', sans-serif;
+    font-size: 100px;
+    color: white;
+}
+</style>
